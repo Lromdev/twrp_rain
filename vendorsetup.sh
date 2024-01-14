6537dbc6bac384f4fb44a00ed13fec164e8da8e9
Lromdev <153156442+Lromdev@users.noreply.github.com>
2024-01-14 22:16:20 +0300

    Update vendorsetup.sh
    kernel304

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,2 @@
 # Clone kernel tree (Sea kernel by Asyanx)
-git clone https://github.com/Asyanx/Sea_Kernel-Fog kernel/xiaomi/fog
-
-# Kernel fixups
-cd kernel/xiaomi/fog
-git reset --hard f1b696
-sed -i 's+R¹.+R0/Upstream+' arch/arm64/configs/vendor/fog-perf_defconfig # rebrand to fix kernel compilation issue during build
-cd ../../../
+git clone https://github.com/Lromdev/kernel304 kernel/xiaomi/fog
